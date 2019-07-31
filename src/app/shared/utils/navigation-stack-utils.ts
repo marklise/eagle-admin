f4865ebcbfff470fdc2a3c5060b686b0e42769aa
Daniel Truong <daniel@dtsoftware.ca>
2019-07-31 16:00:11 -0700

    Revised breadcrumbs and back url service

---
+++ src/app/shared/utils/navigation-stack-utils.ts
@@ -0,0 +1,100 @@
+import { Injectable } from '@angular/core';
+import { StorageService } from 'app/services/storage.service';
+
+@Injectable()
+export class NavigationStackUtils {
+    constructor(private storageService: StorageService) { }
+
+    public getNavigationStack() {
+        if (this.storageService.state.navigationStack) {
+            return this.storageService.state.navigationStack;
+        } else {
+            return null;
+        }
+    }
+
+    public getLastBackUrl() {
+        let stack = this.getNavigationStack();
+        if (stack) {
+            return stack[stack.length - 1].backUrl;
+        } else {
+            return null;
+        }
+    }
+
+    public getLastNavigationObject() {
+        let stack = this.getNavigationStack();
+        if (stack) {
+            return stack[stack.length - 1];
+        } else {
+            return null;
+        }
+    }
+
+    /*
+    Example of a back Url array:
+    ['/p', this.project._id, 'edit']
+
+    Example of a breadcrumbs array:
+    Note that the objects in the array contain a route and a label.
+    [
+        {
+            route: ['/projects'],
+            label: 'All Projects'
+        },
+        {
+            route: ['/p', this.project._id],
+            label: this.project.name
+        },
+        {
+            route: ['/p', this.project._id, 'edit'],
+            label: 'Edit'
+        }
+    ]
+    */
+    public pushNavigationStack(backUrl: Array<String>, breadcrumbs: Array<Object>, componentId: String = null) {
+        let navigationObject = {
+            backUrl: backUrl,
+            breadcrumbs: breadcrumbs,
+            componentId: componentId
+        };
+        let stack = this.getNavigationStack();
+        if (stack) {
+            stack.push(navigationObject);
+            this.storageService.state.navigationStack = stack;
+        } else {
+            this.storageService.state.navigationStack = [navigationObject];
+        }
+    }
+
+    public popNavigationStack() {
+        let stack = this.getNavigationStack();
+        if (stack) {
+            let stackObject = stack.pop();
+            if (stack.length === 0) {
+                this.clearNavigationStack();
+                stackObject = null;
+            } else {
+                this.storageService.state.navigationStack = stack;
+            }
+            return stackObject;
+        } else {
+            this.clearNavigationStack();
+            return null;
+        }
+    }
+
+    public navigateBreadcrumb(breadcrumb, index, router) {
+        let stack = this.getNavigationStack();
+        for (let i = stack.length; i >= index; i--) {
+            if (this.popNavigationStack() == null) {
+                break;
+            }
+        }
+        router.navigate(breadcrumb.route);
+    }
+
+    public clearNavigationStack() {
+        this.storageService.state.navigationStack = null;
+    }
+}
